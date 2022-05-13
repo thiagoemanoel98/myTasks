613cd31d45fc70e7f3ca4288d4f337d6f3a07106
Thiago Emanoel <thiagoemanoel448@gmail.com>
2022-05-13 11:32:49 -0300

    Primeiro Commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import {
+  Platform,
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
+
+export const Home = () => {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome Dev</Text>
+        <TextInput
+          placeholderTextColor="#555"
+          placeholder="Nova tarefa..."
+          style={styles.input}
+        />
+
+        <TouchableOpacity activeOpacity={0.7} style={styles.button}>
+          <Text style={styles.buttonText}>Adicionar</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.titleTasks}>Minhas Tarefas</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#121214',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#121214',
+    paddingHorizontal: 30,
+    paddingVertical: 50,
+  },
+  title: {
+    color: '#f1f1f1',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#29292e',
+    color: '#f1f1f1',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 20,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: '#eba417',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#121214',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  titleTasks: {
+    color: '#f1f1f1',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginVertical: 50,
+  },
+});
